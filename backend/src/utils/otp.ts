52c3dd5d0f27551cb098cec1293ab5024a34c7f0
ahmed ashraf <amedashraf780@gmail.com>
2026-06-19 05:26:34 +0300

    attempt 5 to fix timeout error from nodemailer: use resend

--- backend/src/utils/otp.ts
+++ backend/src/utils/otp.ts
@@ -1,16 +1,18 @@
 import config from "./../config/config";
 import { transporter } from "../config/nodemailer";
+import { resend } from "../config/resend";
 
 export const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
 export const sendOTPEmail = async (recipientEmail: string, otp: string): Promise<boolean> => {
-  const mailOptions = {
-    from: config.mail_user,
-    to: recipientEmail,
-    subject: "Your OTP Code for Verification",
-    html: `
+  try {
+    await resend.emails.send({
+      from: config.mail_user,
+      to: recipientEmail,
+      subject: "Your OTP Code for Verification",
+      html: `
       <div style="font-family: Arial, sans-serif; padding: 20px;">
         <h2>Email Verification</h2>
         <p>Your One-Time Password (OTP) for verification is:</p>
@@ -18,15 +20,7 @@ export const sendOTPEmail = async (recipientEmail: string, otp: string): Promise
         <p>This OTP is valid for a limited time. Do not share it with anyone.</p>
       </div>
     `,
-  };
-
-  try {
-    const ok = await Promise.race([
-      await transporter.sendMail(mailOptions),
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("Email timeout")), 10000)
-      ),
-    ]);
+    });
     return true;
   } catch (error) {
     console.error("Error sending OTP email:", error);
