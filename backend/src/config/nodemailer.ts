78ce9e2b9ae4b63f842f9d76bca87648e0ecd7e6
ahmed ashraf <amedashraf780@gmail.com>
2026-06-19 04:56:28 +0300

    attempt 1 to fix timeout error from nodemailer

--- backend/src/config/nodemailer.ts
+++ backend/src/config/nodemailer.ts
@@ -3,8 +3,10 @@ import config from './config';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
+  secure: true,
   auth: {
     user: config.mail_user,
     pass: config.mail_password
-  }
+  },
+  connectionTimeout: 10000,
 });
